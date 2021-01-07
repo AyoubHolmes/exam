2180af649c7f9015a63caad2b776de5bdbb655e0
Ayoub <boulbaz147@gmail.com>
2021-01-07 23:21:16 +0100

    msg

--- mine/get_next_line.c
+++ mine/get_next_line.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
-int ft_strlen(char *s)
+
+int ft_strlen(const char *s)
 {
-    int i = 0;
-    if(s)
+    int i;
+
+    if (s)
     {
         while(s[i])
             i++;
@@ -14,15 +17,15 @@ int ft_strlen(char *s)
 
 char *ft_strdup(const char *s)
 {
-    char *tmp;
     int len = 0;
     int i = 0;
+    char *tmp;
 
-    while (s[len])
+    while(s[len])
         len++;
-    if(!(tmp = malloc(len + 1)))
+    if(!(tmp = (char *)malloc(len + 1)))
         return (NULL);
-    while (i < len)
+    while(i < len)
     {
         tmp[i] = s[i];
         i++;
@@ -31,38 +34,12 @@ char *ft_strdup(const char *s)
     return (tmp);
 }
 
-char *ft_strjoin(const char *s1, const char *s2)
-{
-    char *tmp;
-    int i = 0;
-    int j = 0;
-
-    if (s1 == NULL)
-        return (ft_strdup(s2));
-    if (s2 == NULL)
-        return (ft_strdup(s1));
-    if(!(tmp = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
-        return(NULL);
-    while (s1[i])
-    {
-        tmp[i] = s1[i];
-        i++;
-    }
-    while (s2[j])
-    {
-        tmp[i + j] = s1[j];
-        j++;
-    }
-    tmp[i + j] = '\0';
-    return (tmp);
-}
-
 char *ft_substr(char const *s, unsigned int start, int len)
 {
-    int i = 0;
     char *tmp;
+    int i = 0;
 
-    if(s == NULL)
+    if (s == NULL)
         return (NULL);
     if (!(tmp = malloc(len + 1)))
         return (NULL);
@@ -76,15 +53,107 @@ char *ft_substr(char const *s, unsigned int start, int len)
     return (tmp);
 }
 
-char *ft_strchr(const char *s, int c)
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    char *tmp;
+    int i = 0;
+    int j = 0;
+
+    if (s1 == NULL)
+        return ft_strdup(s2);
+    if (s2 == NULL)
+        return ft_strdup(s1);
+    if (!(tmp = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+        return (NULL);
+    while (s1[i])
+    {
+        tmp[i] = s1[i];
+        i++;
+    }
+    while (s2[j])
+    {
+        tmp[i + j] = s2[j];
+        j++;
+    }
+    tmp[i + j] = '\0';
+    return (tmp);
+}
+
+char *ft_strchr(char *s, int c)
 {
     int i = 0;
     char ch = c;
 
-    while(s[i] && s[i] != ch)
+    while (s[i] && s[i] != ch)
         i++;
     if (s[i] == ch)
         return ((char *)&s[i]);
-    else
+    else 
         return (NULL);
+}
+
+int read_line(char **str, char **line)
+{
+    int i = 0;
+    char *tmp;
+
+    if (*str)
+    {
+        while((*str)[i] && (*str)[i] != '\n')
+            i++;
+        if ((*str)[i] == '\n')
+        {
+            *line = ft_substr(*str, 0, i);
+            tmp = *str;
+            *str = ft_strdup(*str + i + 1);
+            free(tmp);
+        }
+        else if((*str)[i] == '\0')
+        {
+            *line = ft_strdup(*str);
+            free(*str);
+            *str = NULL;
+            return (0);
+        }
+    }
+    return (1);
+}
+
+int get_next_line(char **line)
+{
+    int r;
+    char *tmp;
+    static char *str;
+    char buff[1025];
+
+    while((r = read(0, buff, 1024)) > 0)
+    {
+        buff[r] = '\0';
+        tmp = str;
+        str = ft_strjoin(str, buff);
+        free(tmp);
+        if (ft_strchr(str, '\n'))
+            break;
+    }
+    if(r < 0)
+        return (-1);
+    if (!r && (str == NULL || str[0] == '\0'))
+    {
+        *line = ft_strdup("");
+        return (0);
+    }
+    return (read_line(&str, line));
+}
+
+int		main(int argc, char **argv)
+{
+	char	*line;
+    int i = 0;
+
+	while ((i = get_next_line(&line)) == 1)
+	{
+		printf("%s\n" ,line);
+		free(line);
+	}
+    printf("%s" ,line);
 }
